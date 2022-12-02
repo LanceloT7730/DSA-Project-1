28edd5edffd4fc4f72dc96a8fcd8858ee5c53a7b
Doniyorbek <ac2952602@akfauniversity.org>
2022-12-02 17:25:01 +0500

    Initial Commit

---
+++ project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by DONIYORBEK IBROKHIMOV on 02/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

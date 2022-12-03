eb8ec0f32c3938b3ce9b8b50d7d5e18a5e0c86ce
210046 <ac2952602@akfauniversity.org>
2022-12-03 20:00:24 +0500

    final mod

--- project1/main.cpp
+++ project1/main.cpp
@@ -8,23 +8,23 @@ class Child {
     string name;
 public:
     Child *nextChild;
-    
-    Child(){
+
+    Child() {
         nextChild = nullptr;
     }
-    
+
     int getAge() {
         return age;
     }
-    
+
     void setAge(int &age) {
         this->age = age;
     }
-    
+
     string getName() {
         return name;
     }
-    
+
     void setName(string &name) {
         this->name = name;
     }
@@ -34,22 +34,20 @@ class Spouse {
     int age;
     string name;
     int num_children;
-    
+
     Child *childHead;
-    //Child *newChild = new Child;
-    
+
     void insertChildAtFront(int age, string name) {
 //i=0; i<num_children
-Child *newChild = new Child;
+        auto *newChild = new Child;
         newChild->setAge(age);
         newChild->setName(name);
         newChild->nextChild = childHead;
         childHead = newChild;
     }
-    
+
 public:
     Spouse() {
-
         cout << "Enter the number of childs" << endl;
         cin >> num_children;
         if (num_children <= 0) num_children = 0;
@@ -58,60 +56,61 @@ public:
             childrenDetails(num_children);
         }
     }
+
     int getAge() {
         return age;
     }
-    
+
     auto getChildHead() {
         return childHead;
     }
-    
+
     unsigned int get_child_num() {
         return num_children;
     }
-    
+
     void setAge(int &age) {
         this->age = age;
     }
-    
+
     string getName() {
         return name;
     }
-    
+
     void setName(string &name) {
         this->name = name;
     }
 
-    
+
     void childrenDetails(int lim_num_children) {
         int i = 1;
-   
+
         while (i <= lim_num_children) {
-            cout << "Enter the child "<< i << " name->";
+            cout << "Enter the child " << i << " name->";
             string child_name;
             cin >> child_name;
-    
-            cout << "Enter the child "<< i << " age->";
+
+            cout << "Enter the child " << i << " age->";
             int child_age = 0;
             cin >> child_age;
             insertChildAtFront(child_age, child_name);
             i++;
         }
-        
+
     }
 
     void printChildrenDetails() {
         Child *temp = childHead;
         int i = num_children;
-        while(temp) {
-            cout <<"Child " << i <<" name is " << temp->getName()
-            << " and this child's age is " << temp->getAge() << endl;
-            
+        while (temp) {
+            cout << "Child " << i << " name is " << temp->getName()
+                 << " and this child's age is " << temp->getAge() << endl;
+
             temp = temp->nextChild;
             i--;
         }
     }
-    
+
     ~Spouse() {
         Child *currChild = childHead;
         while (currChild) {
@@ -120,7 +119,7 @@ public:
             currChild = childHead;
         }
     }
-    
+
 };
 
 struct Employee {
@@ -128,7 +127,7 @@ struct Employee {
     string employee_name, designation, email;
     int employee_id, age;
     Employee *nextEmployee;
-    
+
     Employee() {
         cout << "Enter 1 if employee is married, 0 otherwise" << endl;
         cin >> isSpouseAvailable;
@@ -138,74 +137,73 @@ struct Employee {
             spouseDetails();
         }
     }
-    
+
     ~Employee() {
         delete spouseLink;
     }
 
-    void printChildrenDetails()
-    {
+
+    void printChildrenDetails() {
         spouseLink->printChildrenDetails();
     }
 
-    
+
 private:
     Spouse *spouseLink;
-    
+
     void spouseDetails() {
         cout << "Enter the spouse's name->";
         string spouse_name;
         cin >> spouse_name;
         spouseLink->setName(spouse_name);
-        
+
         cout << "Enter the spouse's age->";
         int spouse_age = 0;
         cin >> spouse_age;
         spouseLink->setAge(spouse_age);
     }
-    
+
 };
 
 struct List {
     List() {
         head = nullptr;
     }
-    
+
     void insertEmployeeAtFront(string employee_name, string designation,
                                string email, int employee_id, int age) {
-        
+
         Employee *newEmployee = new Employee;
         newEmployee->employee_name = employee_name;
         newEmployee->designation = designation;
         newEmployee->email = email;
         newEmployee->employee_id = employee_id;
         newEmployee->age = age;
-        
+
         newEmployee->nextEmployee = head;
         head = newEmployee;
     }
-    
+
     void printAllEmpChildDetails() {
         Employee *curr = head;
-        while(curr)
-        {
+        while (curr) {
             curr->printChildrenDetails();
             curr = curr->nextEmployee;
         }
     }
-    
+
 private:
     Employee *head;
 };
 
-int main(){
+int main() {
     List list1;
-    list1.insertEmployeeAtFront("Tim","designation1","Example1@gmail.com", 210045, 23 );
-    list1.insertEmployeeAtFront("Kruz","designation2","Example2@gmail.com", 210046, 25 );
-    list1.insertEmployeeAtFront("Kevin","designation3","Example3@gmail.com", 210047, 27 );
-    
+    list1.insertEmployeeAtFront("Tim", "designation1", "Example1@gmail.com", 210045, 23);
+    list1.insertEmployeeAtFront("Kruz", "designation2", "Example2@gmail.com", 210046, 25);
+    list1.insertEmployeeAtFront("Kevin", "designation3", "Example3@gmail.com", 210047, 27);
+
     list1.printAllEmpChildDetails();
 
-    
+
     return 0;
 }
